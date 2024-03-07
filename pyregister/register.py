9299d73e95a6e82f31cde7ee8dd3e09ccc5b3afc
Arthur Rocha <nmqvl@kali.kali>
2024-03-06 23:57:00 -0300

    v1.1.0

--- pyregister/register.py
+++ pyregister/register.py
@@ -2,7 +2,7 @@
 import os
 import csv
 from time import sleep
-
+import logging
 import pyautogui
 
 
@@ -13,113 +13,125 @@ def clear_screen():
     else:
         os.system("clear")
 
+# Registra todos os logs do programa e printa no terminal
+class Logger:
+    def __init__(self, filename="log.txt", level=logging.INFO):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(level)
+
+        formater = logging.Formatter("%(asctime)s / %(levelname)s: %(message)s")
+
+        file_handler = logging.FileHandler(filename)
+        file_handler.setFormatter(formater)
+
+        self.logger.addHandler(file_handler)
+
+    def log(self, message, level=logging.INFO):
+        self.logger.log(level, message)
+        print(message)
+
 
 # Captura as coordenadas do campo desejado
 class UserInteraction:
-    def aler_user(self, message):
-        print(message)
+    def __init__(self, logger):
+        self.logger = logger
+
+    def alert_user(self, message):
+        self.logger.log(message)
 
     def get_coordinates(self, name):
-        self.aler_user("Você tem 3 segundos pra marcar o ponto desejado...")
+        self.alert_user("Você tem 3 segundos para marcar o ponto desejado...")
         sleep(3)
-
         x, y = pyautogui.position()
-        self.aler_user(f"{name}: Cordernadas {x}, {y} registradas.")
+        self.alert_user(f"{name}: Coordenadas {x}, {y} registradas.")
         return x, y
 
-
 class Registrar:
-    def __init__(self, input_file, margin, wait_time, max_iterations=None):
+    def __init__(self, input_file, margin, wait_time, max_iterations=None, logger=None):
         self.input_file = input_file
         self.margin = margin
         self.wait_time = wait_time
         self.max_iterations = max_iterations
-
-        # Segurança contra falhas (delay de resposta)
+        self.logger = logger
         self.mouse_speed = 0.5
         pyautogui.FAILSAFE = True
 
-        # DADOS ESPECÍFICOS DESSE FLUXO!!!
+        # Coordenadas dos campos
         self.DADOS_FISCAIS = [706, 307]
         self.SAVE = [562, 209]
+        self.CONFIRMATION = [669,443]
         COLUMNS = ["nome", "valor", "margin", "cst", "taxa", "ncm"]
-        self.user_interaction = UserInteraction()
-
+        self.user_interaction = UserInteraction(logger)
 
         # Estrutura dos valores
         self.FIELDS = []
 
-        # Armazena as coordenadas do field e valores do produto na estrutura
+        # Armazena as coordenadas do campo e valores do produto na estrutura
         for col in COLUMNS:
             coordinates = self.user_interaction.get_coordinates(col)
-            self.FIELDS.append({ "axis": coordinates, "info": col })
-
+            self.FIELDS.append({"axis": coordinates, "info": col})
 
     def register_product(self, product):
-        print(f'''
-            -------------------------------------------------
-                    {product}
-            -------------------------------------------------
-            ''')
-        
         for field in self.FIELDS:
+            x, y = field["axis"]
+
             # Move o mouse para os campos escolhidos
-            x = field["axis"][0]
-            y = field["axis"][1]
             sleep(self.wait_time)
             pyautogui.moveTo(x, y, duration=self.mouse_speed)
+
+            # Clica duas vezes no campo
+            pyautogui.click(clicks=2)
             sleep(self.wait_time)
 
-            # Double click
-            print("click 2x")
-            for i in range(0, 2):
-                pyautogui.click()
-            sleep(self.wait_time)
-
-            # Escreve o self.margin
+            # Escreve o valor
             if field["info"] == "margin":
                 pyautogui.write(self.margin)
-                print(f"maigin: {self.margin}")
-                continue
+                self.logger.log(f"margin: {self.margin}")
+            else:
+                key = field["info"]
+                pyautogui.write(product[key].replace('.', ','))
+                self.logger.log(f"{field['info']}: {product[key]}")
 
-            # Escreve os valores
-            key = field["info"]
-            pyautogui.write(product[key].replace('.', ','))
-            print(f"{ field['info'] }: { product[field['info']] }")
+        # Salva o produto
+        self.click_and_wait(self.SAVE)
 
-            sleep(self.wait_time)
-            pyautogui.moveTo(*self.SAVE, duration=self.mouse_speed)
-            # print("save")
+        # Confirma a salvação
+        self.click_and_wait(self.CONFIRMATION, 'Produto salvo!')
 
-        print("----------------------------")
+        self.logger.log("*****--*****--*****-*****--*****--*****--*****--*****")
+        self.logger.log("\n\n")
+
+    def click_and_wait(self, cordinates, action=None):
+        pyautogui.moveTo(*cordinates, duration=self.wait_time)
+        pyautogui.click()
+        sleep(self.wait_time)
+        if action:
+            self.logger.log(action)
 
     def run(self):
         # Lê csv
-        print("Abrindo arquivo...")
+        self.logger.log("Abrindo arquivo...")
         try:
             with open(self.input_file, "r", encoding="utf-8") as file:
                 products = list(csv.DictReader(file))
         except Exception as error:
-             print(f"Ocorreu um erro ao abrir o arquivo CSV {self.input_file} para leitura: {error}")
+            self.logger.log(f"Ocorreu um erro ao abrir o arquivo CSV {self.input_file} para leitura: {error}")
 
-        # Alerta 
+        # Limpa o terminal
+        clear_screen()
+
+        # Alerta
         pyautogui.alert("ATENÇÃO!!: Não use mouse e tecla, pode atrapalhar o funcionamento.\nO RPA vai ser executado.")
+        self.logger.log("Iniciando...")
+        self.logger.log("\n")
 
         # Prepara o ambiente
-        pyautogui.moveTo(*self.SAVE, duration=self.mouse_speed)
-        pyautogui.click()
-        sleep(self.wait_time)
-
-        pyautogui.moveTo(*self.DADOS_FISCAIS, duration=self.mouse_speed)
-        pyautogui.click()
-        sleep(self.wait_time)
-
+        self.click_and_wait(self.DADOS_FISCAIS)
 
         # Cadastra cada produto
         count_iteration = 0
         for product in products:
             self.register_product(product)
-
             count_iteration += 1
             if self.max_iterations is not None and count_iteration == self.max_iterations:
                 break
--- register_product.py
+++ register_product.py
@@ -2,6 +2,13 @@ import pyregister
 import sys
 import os
 
+def get_download_directory(filename):
+    user_home = os.path.expanduser("~")
+    if os.name == "nt" or os.name == "posix":
+        return os.path.join(user_home, "Downloads", filename)
+    else:
+        return os.path.join(os.getcwd(), filename)
+
                 
 def main():
     if len(sys.argv) < 3:
@@ -10,17 +17,15 @@ def main():
 
     print("Iniciando...")
     # Obtêm o caminho
-    current_directory = os.getcwd()
-    full_path = os.path.join(current_directory, sys.argv[1])
-
+    full_path = get_download_directory(sys.argv[1])
     margin = sys.argv[2]
     wait_time = float(sys.argv[3])
     max_iterations = None
-    
+    logger = pyregister.Logger()
     if len(sys.argv) == 5:
         max_iterations = int(sys.argv[4])
 
-    product_registrar = pyregister.Registrar(full_path, margin, wait_time, max_iterations)
+    product_registrar = pyregister.Registrar(full_path, margin, wait_time, max_iterations, logger)
     product_registrar.run()
 
 
